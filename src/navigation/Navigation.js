2bfd49695e3d2804c447073f7045dd13732f89ed
cobigo <cobigomorgane@Mac.lan>
2024-11-13 17:17:33 +0100

    gh-pages branch

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import Home from "../pages/home/Home";
@@ -10,7 +10,11 @@ const Navigation = () => {
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/portfolio-de-morgane" element={<Home />} />
+          <Route
+            path="*"
+            element={<Navigate replace to="/portfolio-de-morgane" />}
+          />
         </Routes>
       </main>
       <Footer />
